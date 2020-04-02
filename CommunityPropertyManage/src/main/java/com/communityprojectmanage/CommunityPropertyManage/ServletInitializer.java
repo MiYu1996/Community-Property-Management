521190c6d42c9e69b8efe3bd0136142eb0a8301d
MiYu1996 <47319583+MiYu1996@users.noreply.github.com>
2020-04-02 16:57:31 -0700

    Add files via upload

---
+++ CommunityPropertyManage/src/main/java/com/communityprojectmanage/CommunityPropertyManage/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.communityprojectmanage.CommunityPropertyManage;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CommunityPropertyManageApplication.class);
+	}
+
+}
---
+++ CommunityPropertyManage/src/test/java/com/communityprojectmanage/CommunityPropertyManage/CommunityPropertyManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.communityprojectmanage.CommunityPropertyManage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityPropertyManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
